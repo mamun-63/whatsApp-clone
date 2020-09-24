b4ab1f7b848303b99c8f0d58e1c42efb457e735a
Mamun <mamun.cse63@gmail.com>
2020-09-25 04:39:55 +0600

    created the front display and formed the prototype of sidebar

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrQDrW_YdPtV9DbC6ZjCncGhkejzvROdw",
+  authDomain: "whatsapp-clone-499ab.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-499ab.firebaseio.com",
+  projectId: "whatsapp-clone-499ab",
+  storageBucket: "whatsapp-clone-499ab.appspot.com",
+  messagingSenderId: "132262868783",
+  appId: "1:132262868783:web:5dacc46f85996c1285d3da",
+  measurementId: "G-TV5JBN3BKQ"
+};
