3bd963bd563cfe1eab624118a44d6126db5b1ddb
Mamun <mamun.cse63@gmail.com>
2020-09-26 03:09:03 +0600

    setting up the firebase.js and created a rooms collection manually in firestore to create new rooms

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase' 
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCrQDrW_YdPtV9DbC6ZjCncGhkejzvROdw",
@@ -9,3 +11,12 @@ const firebaseConfig = {
   appId: "1:132262868783:web:5dacc46f85996c1285d3da",
   measurementId: "G-TV5JBN3BKQ"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+
+const auth = firebase.auth()  // no need the config file, so not using firebaseAPP.auth(), it comes from npm firebase pakage
+const provider = new firebase.auth.GoogleAuthProvider() // creating new instance of Google authentication
+
+export {auth, provider}
+export default db
