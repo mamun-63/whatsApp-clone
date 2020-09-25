d8e0cbce0410dd04e5e28b448a8d7dfc100feed7
Mamun <mamun.cse63@gmail.com>
2020-09-26 01:35:26 +0600

    chat body footer is completed, needs to fire some logics now

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,5 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import { Mic, AttachFile, InsertEmoticon, InsertEmoticonOutlined, MoreVert, SearchOutlined } from '@material-ui/icons'
 import React, { useEffect, useState } from 'react'
 import './Chat.css'
 
@@ -9,6 +9,10 @@ function Chat() {
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000))
   },[])
+
+  const sendMessage = (e) => {
+
+  }
 
 
   return (
@@ -43,7 +47,12 @@ function Chat() {
       </div>
 
       <div className="chat__footer">
-
+        <InsertEmoticonOutlined />
+        <form action="">
+          <input placeholder="Type a message" type="text"/>
+          <button onClick={sendMessage} type="submit">Send a message</button>
+        </form>
+        <Mic />
       </div>
     </div>
   )
